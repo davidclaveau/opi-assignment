dba1177b84d5fb0174ead4bcbad0be3f9370b0c8
davidclaveau <daveclaveau@gmail.com>
2021-08-27 10:38:28 -0700

    Create Dashboard component

---
+++ src/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  
+}
+
+const Dashboard = (props: Props) => {
+  return (
+    <div>
+      This is where the dashboard is rendered.
+    </div>
+  )
+}
+
+export default Dashboard
